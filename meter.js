281f91e788cd8b0b04c01cef65ef4016417a4fec
janhajk <janhajk@gmail.com>
2020-04-22 11:22:01 +0000

    optimie logging;

--- meter.js
+++ meter.js
@@ -1,8 +1,11 @@
-var axios = require('axios').default;;
+var axios = require('axios').default;
+
+
 const config = {
    meter: {
       host: process.env.APIOKEX_KEY,
-   }
+   },
+   log: process.env.LOG
 };
 
 
@@ -49,7 +52,9 @@ var Meter = function() {
 
    this.getPAC = function(cb) {
       call(fronius_api.GetInverterRealtimeData, function(err, response, body) {
-         console.log('> IP ' + config.meter.host + '...');
+         if (config.log) {
+            console.log('> IP ' + config.meter.host + '...');
+         }
          var data = JSON.parse(body);
          cb(data.Body.Data.PAC.Values['1']);
       });
@@ -57,9 +62,13 @@ var Meter = function() {
 
    this.getInverter = function(cb) {
       call(fronius_api.GetInverterRealtimeData, function(err, body) {
-         console.log('> IP ' + config.meter.host + '...');
+         if (config.log) {
+            console.log('> IP ' + config.meter.host + '...');
+         }
          var data = JSON.parse(body);
-         console.log(data.Body.Data.DAY_ENERGY);
+         if (config.log) {
+            console.log(data.Body.Data.DAY_ENERGY);
+         }
          cb({ pac: data.Body.Data.PAC.Values['1'], day_energy: data.Body.Data.DAY_ENERGY.Values['1'] });
       });
    };
@@ -67,7 +76,9 @@ var Meter = function() {
    this.getGrid = function(cb) {
       call(fronius_api.GetMeterRealtimeData, function(err, body) {
          var data = JSON.parse(body);
-         console.log(data.Body.Data['0']);
+         if (config.log) {
+            console.log(data.Body.Data['0']);
+         }
          var p = data.Body.Data['0'].PowerReal_P_Sum;
          cb(p);
       });
@@ -76,7 +87,9 @@ var Meter = function() {
    this.getSparePower = function(cb) {
       this.getGrid(function(p) {
          p = -p;
-         console.log('Current Spare Power: ' + p + ' Watt');
+         if (config.log) {
+            console.log('Current Spare Power: ' + p + ' Watt');
+         }
          cb(p);
       });
    };
