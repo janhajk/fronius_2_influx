adc0b64f303a9b287f72d6749ae8802527734b57
janhajk <janhajk@gmail.com>
2020-04-22 06:24:53 +0000

    initial

---
+++ logData.js
@@ -0,0 +1,61 @@
+var meter = require(__dirname + '/meter.js');
+
+const Influx = require("influx");
+
+const config = {
+      influxdb: {
+            host: process.env.SQLHOST,
+            port: process.env.SQLPORT,
+            db: process.env.SQLDB,
+            user: process.env.SQLUSER,
+            password: process.env.SQLPSW,
+      }
+};
+
+
+const influx = new Influx.InfluxDB({
+      host: config.influxdb.host + ':' + config.influxdb.port,
+      database: config.influxdb.db,
+      username: config.influxdb.user,
+      password: config.influxdb.password,
+
+      schema: [{
+            measurement: "powerdata",
+            fields: {
+                  pac: Influx.FieldType.FLOAT,
+                  grid: Influx.FieldType.FLOAT,
+                  total: Influx.FieldType.FLOAT,
+                  day_energy: Influx.FieldType.FLOAT
+            },
+            tags: []
+      }]
+});
+
+let writeData = function() {
+      meter.getInverter(function(dataInverter) {
+            meter.getGrid(function(grid) {
+                  influx
+                        .writePoints(
+                              [{
+                                    measurement: "powerdata",
+                                    fields: {
+                                          pac: dataInverter.pac,
+                                          grid: grid,
+                                          total: dataInverter.pac + grid,
+                                          day_energy: dataInverter.day_energy
+                                    }
+                              }], {
+                                    database: config.influxdb.db,
+                                    precision: "s"
+                              }
+                        )
+                        .catch(err => {
+                              console.error("Error writing data to Influx.");
+                        });
+            });
+      });
+};
+// influx.query('DROP SERIES FROM /.*/').then(results => {
+//   console.log(results)
+// })
+setInterval(writeData, 10000);
---
+++ meter.js
@@ -0,0 +1,70 @@
+var request = require('request');
+const config = {
+   meter: {
+      host: process.env.APIOKEX_KEY,
+   }
+};
+
+
+var fronius_api = {
+   GetSensorRealtimeData: '/solar_api/v1/GetSensorRealtimeData.cgi?Scope=System&DataCollection=NowSensorData',
+   GetInverterRealtimeData: '/solar_api/v1/GetInverterRealtimeData.cgi?Scope=System',
+   GetActiveDeviceInfo: '/solar_api/v1/GetActiveDeviceInfo.cgi?DeviceClass=System',
+   GetMeterRealtimeData: '/solar_api/v1/GetMeterRealtimeData.cgi?Scope=System'
+};
+
+
+
+var Meter = function() {
+
+   var call = function(path, cb) {
+      var url = 'http://' + config.meter.host + '/' + path;
+      request(url, function(e, response, body) {
+         if (e) {
+            console.log('error in request ' + url);
+            console.log(e);
+            cb(e);
+         }
+         else {
+            cb(null, response, body);
+         }
+      });
+   };
+
+   this.getPAC = function(cb) {
+      call(fronius_api.GetInverterRealtimeData, function(err, response, body) {
+         console.log('> IP ' + config.meter.host + '...');
+         var data = JSON.parse(body);
+         cb(data.Body.Data.PAC.Values['1']);
+      });
+   };
+   
+   this.getInverter = function(cb) {
+      call(fronius_api.GetInverterRealtimeData, function(err, response, body) {
+         console.log('> IP ' + config.meter.host + '...');
+         var data = JSON.parse(body);
+         console.log(data.Body.Data.DAY_ENERGY);
+         cb({pac: data.Body.Data.PAC.Values['1'], day_energy: data.Body.Data.DAY_ENERGY.Values['1'] });
+      });
+   };
+
+   this.getGrid = function(cb) {
+      call(fronius_api.GetMeterRealtimeData, function(err, response, body) {
+         var data = JSON.parse(body);
+         console.log(data.Body.Data['0']);
+         var p = data.Body.Data['0'].PowerReal_P_Sum;
+         cb(p);
+      });
+   };
+   
+   this.getSparePower = function(cb) {
+      this.getGrid(function(p){
+         p = -p;
+         console.log('Current Spare Power: ' + p + ' Watt');
+         cb(p);
+      });
+   };
+
+};
+
+module.exports = new Meter();
